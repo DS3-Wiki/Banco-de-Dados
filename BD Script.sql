239c65a4caaa1b88a0c4bb97176b0d1def329b66
LccsBueno <125739142+LccsBueno@users.noreply.github.com>
2023-05-22 23:14:43 -0300

    Add files via upload

--- BD Script.sql
+++ BD Script.sql
@@ -1,18 +1,16 @@
 use soulsbuilds;
 
-create user 'insert'@'localhost' identified by 'nsert';
+create user 'insert'@'localhost' identified by 'insert';
 	grant insert on * to 'insert';
-create user 'sekect'@'kicakgist' identified by 'select';
+    
+create user 'select'@'localhost' identified by 'select';
 	grant select on * to 'select';
     
 create table boss_status (
-fkBoss int,
-fkimmunity int,
-fkresistance int,
-fkweakness int,
-	foreign key (fkimmunity) references Status (id_status),
-	foreign key (fkresistance) references Status (id_status),
-	foreign key (fkweakness) references Status (id_status)	
+fk_boss int,
+effect_status varchar(45),
+fk_status int,
+	constraint fk_status foreign key (fk_status) references status (fk_status)
 );
 
 insert into boss_status values 
@@ -26,23 +24,23 @@ status.status
 	from bosses join boss_status
 	on fkBoss = id_boss
     join soulsbuilds.status
-		on id_status in (fkresistance, fkweakness, fkimmunity);
-			
+		on id_status = fk_status;
 	
 create table usuario (
-id_user int primary key,
+id_user int primary key auto_increment,
 nome varchar(45),
 email varchar(80),
 password varchar(45),
 ds_experience int
 );
 
-
 create table build (
 id_build int primary key auto_increment,
 name varchar(45),
 perks varchar(45),
-level int
+level int,
+fk_user int,
+	constraint fk_user foreign key(fk_user) references usuario (id_user)
 );
 
 create table dificuldade (
@@ -53,4 +51,29 @@ dificuldade int,
 	foreign key (fk_boss) references bosses (id_boss),
 	foreign key (fk_build) references build (id_build),
 	foreign key (fk_user) references usuario (id_user)
+);
+
+create table status(
+id_status int primary key auto_increment,
+status varchar(45)
+);
+
+create table perks(
+id_perks int primary key auto_increment,
+vigor int,
+attunement int,
+endurance int,
+vitality int,
+strength int,
+dexterity int,
+intelligence int,
+faith int,
+luck int
+);
+
+create table bosses(
+id_boss int primary key auto_increment,
+name varchar(45),
+health_points int,
+souls int
 );
