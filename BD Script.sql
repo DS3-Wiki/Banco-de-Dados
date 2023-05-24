2e6b2b706af2b8a48b9011b78bafc92224729725
LccsBueno <125739142+LccsBueno@users.noreply.github.com>
2023-05-23 22:53:06 -0300

    Add files via upload

--- BD Script.sql
+++ BD Script.sql
@@ -1,3 +1,5 @@
+create soulsbuilds;
+
 use soulsbuilds;
 
 create user 'insert'@'localhost' identified by 'insert';
@@ -14,8 +16,17 @@ fk_status int,
 );
 
 insert into boss_status values 
-	(1, 7, 3, 1);
+	(1, );
 
+insert into status values
+	(1, 'Dark'),
+	(2, 'Bleed'),
+	(3, 'Fire'),
+	(4, 'Frost'),
+	(5, 'Poison'),
+	(6, 'Toxic'),
+	(7, 'Magic'),
+	(8, 'Lighning');
 
 select 
 bosses.name,
@@ -77,3 +88,26 @@ name varchar(45),
 health_points int,
 souls int
 );
+
+insert into bosses values
+	(null, 'Iudex Gundyr', 1037, 3000),
+	(null, 'Vordt of the Boreal Valley', 1328, 3000),
+	(null, 'Curse-Rotted Greatwood', 5405, 7000),
+	(null, 'Crstal Sage', 2723, 8000),
+	(null, 'Abyss Watchers', 1548, 18000),
+	(null, 'Deacons of the Deep', 4099, 13000),
+	(null, 'High Lord Wolnir', 15041, 22000),
+	(null, 'Old Demon King', 5301, 25000),
+	(null, 'Pontiff Sulyvahn', 5106, 28000),
+	(null, 'Yhorm the Giant', 27822, 36000),
+	(null, 'Aldrich, Devourer of Gods', 4727, 50000),
+	(null, 'Dancer of the Boreal Valley', 5111, 60000),
+	(null, 'Dragonslayer Armour', 4581, 48000),
+	(null, 'Oceiros, the Consumed King', 8087, 58000),
+	(null, 'Champion Gundyr', 4956, 60000),
+	(null, 'Lothric, Younger Prince', 4294, 85000),
+	(null, 'Ancient Wyvern', 7873, 70000),
+	(null, 'Nameless King', 4577, 80000),
+	(null, 'Soul of Cinder', 6557, 100000);
+    
+    
